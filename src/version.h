c4650f31b98ca0b32d5503038d5e8d6ae49e679c
Zilbercoin <83983401+Zilbercoin@users.noreply.github.com>
2021-05-29 22:55:02 +0500

    Update protocol version

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@ static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR
                          +     100 * CLIENT_VERSION_REVISION
-                         +       1 * CLIENT_VERSION_BUILD;
+                         +       2 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 70510;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60019;
+static const int PROTOCOL_VERSION = 60020;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60016;
+static const int MIN_PEER_PROTO_VERSION = 60020;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
